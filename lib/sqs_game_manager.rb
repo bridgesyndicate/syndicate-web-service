897ad1fe64e47057fa26be64aa21998964c9a2cc
Kenneth Berland <ken@hero.net>
2021-08-06 15:34:52 -0700

    make sqs enqueue for any queue

--- lib/sqs_game_manager.rb
+++ lib/sqs_game_manager.rb
@@ -1,24 +1,22 @@
 require 'aws-sdk-sqs'
 
 class SqsGameManager
-  attr_accessor :client, :queue_name
+  attr_accessor :client
 
   def initialize()
-
-    @queue_name = "syndicate_#{SYNDICATE_ENV}_games"
 
     @client = Aws::SQS::Client.new(region: AwsCredentials.instance.region,
                                         credentials: AwsCredentials.instance.credentials
                                         )
   end
 
-  def create_queue
-    create_queue_impl unless @client.list_queues.queue_names.include?(@queue_name)
+  def create_queue(queue_name)
+    create_queue_impl(queue_name) unless @client.list_queues.queue_names.include?(queue_name)
   end
 
-  def create_queue_impl
+  def create_queue_impl(queue_name)
     return @client.create_queue({
-                                  queue_name: @queue_name
+                                  queue_name: queue_name
                                 })
   end
 
@@ -30,9 +28,9 @@ class SqsGameManager
 
   # queue_name_resp = client.get_queue_url({ queue_name: @queue_name }) # want to avoid another service call
 
-  def enqueue(message)
+  def enqueue(queue_name, message)
     @client.send_message({
-                           queue_url: get_queue_url(@queue_name),
+                           queue_url: get_queue_url(queue_name),
                            message_body: message
                          })
   end
