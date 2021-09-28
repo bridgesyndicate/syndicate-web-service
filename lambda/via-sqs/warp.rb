e4da5a34cf6ce0b853fb6a3cfbba7add7a8e93d9
Kenneth Berland <ken@hero.net>
2021-09-28 07:43:48 -0700

    string is already parsed from JSON

--- lambda/via-sqs/warp.rb
+++ lambda/via-sqs/warp.rb
@@ -5,10 +5,8 @@ require 'lib/rabbit_client'
 
 def handler(event:, context:)
 
-  puts event['body']
-
   # Warp folks back to the lobby as this game is over on update
-  minecraft_uuids = JSON.parse(event['body'])
+  minecraft_uuids = event['body']
   minecraft_uuids.each do |id|
     $rabbit_client.send_player_to_host(id, 'lobby', '')
   end
