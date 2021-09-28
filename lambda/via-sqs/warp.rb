eaa264a1d4b77ae3a74d9fae57f0b935a6b2c49e
Kenneth Berland <ken@hero.net>
2021-09-28 07:39:08 -0700

    typo

--- lambda/via-sqs/warp.rb
+++ lambda/via-sqs/warp.rb
@@ -8,7 +8,7 @@ def handler(event:, context:)
   puts event['body']
 
   # Warp folks back to the lobby as this game is over on update
-  minecraft_uuids = JSON.parse(puts event['body'])
+  minecraft_uuids = JSON.parse(event['body'])
   minecraft_uuids.each do |id|
     $rabbit_client.send_player_to_host(id, 'lobby', '')
   end
