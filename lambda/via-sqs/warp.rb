4a584ea1a99fb7b551e3ff7b258fd32a2f90ac9d
Kenneth Berland <ken@hero.net>
2021-09-29 16:45:41 -0700

    parse the message

--- lambda/via-sqs/warp.rb
+++ lambda/via-sqs/warp.rb
@@ -5,9 +5,10 @@ require 'lib/rabbit_client'
 
 def handler(event:, context:)
 
-  puts event.inspect
   # Warp folks back to the lobby as this game is over on update
-  minecraft_uuids = event['body']
+  minecraft_uuids = event["Records"]
+                      .map{|record| JSON.parse(record['body']) }
+                      .flatten
   minecraft_uuids.each do |id|
     $rabbit_client.send_player_to_host(id, 'lobby', '')
   end
