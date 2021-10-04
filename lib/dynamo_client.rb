478f21860a0adb2be085af9be6db8f7f958dc378
Kenneth Berland <ken@hero.net>
2021-10-04 16:23:32 -0700

    add require

--- lib/dynamo_client.rb
+++ lib/dynamo_client.rb
@@ -5,6 +5,7 @@ require 'lib/helpers'
 require 'lib/mock_dynamodb_game_manager'
 require 'lib/mock_dynamodb_helpers'
 require 'lib/mock_dynamodb_kick_code_manager'
+require 'lib/object_not_found'
 
 $ddb_game_manager    =  SYNDICATE_ENV == 'test' ? MockDynamodbGameManager.new() : DynamodbGameManager.new()
 $ddb_kick_code_manager = SYNDICATE_ENV == 'test' ? MockDynamodbKickCodeManager.new() : DynamodbKickCodeManager.new()
