922c2abb2c2adba8b46dfbf52c8a582431567b96
Kenneth Berland <ken@hero.net>
2021-10-04 10:08:55 -0700

    Make kick codes only valid once.

---
+++ console.rb
@@ -0,0 +1,23 @@
+libpath = File.join(File.expand_path(File.dirname(__FILE__)), 'lib')
+$LOAD_PATH.unshift(libpath) unless $LOAD_PATH.include?(libpath)
+dotpath = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift(dotpath) unless $LOAD_PATH.include?(dotpath)
+
+require 'aws_credentials'
+require 'lambda/auth/game/accept/post'
+require 'lambda/auth/game/container_metadata/put'
+require 'lambda/auth/game/post'
+require 'lambda/auth/game/put'
+require 'lambda/auth/register/by-kick-code/post'
+require 'lambda/auth/user/by-minecraft-uuid/get'
+require 'pry'
+
+require 'lib/aws_credentials'
+require 'lib/dynamo_client.rb'
+require 'lib/helpers'
+require 'securerandom'
+
+code = SecureRandom.alphanumeric
+$ddb_kick_code_manager.put(code, SecureRandom.uuid)
+binding.pry;1
+use_once(code)
