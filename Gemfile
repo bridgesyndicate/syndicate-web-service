19fd591a7822b9c5f1a1c2f15ff2f17238ec3996
Kenneth Berland <ken@hero.net>
2021-10-06 17:03:37 -0700

    send the decoded event

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'aws-sdk-dynamodb'
+gem 'aws-sdk-dynamodbstreams'
 gem 'aws-sdk-sqs'
 gem 'bunny'
 gem 'elo_rating', github: 'mxhold/elo_rating', branch: 'master'
--- lambda/via-stream/games.rb
+++ lambda/via-stream/games.rb
@@ -1,13 +1,14 @@
 load 'git_commit_sha.rb'
+require 'aws-sdk-dynamodbstreams'
 require 'json'
 require 'lib/aws_credentials'
 require 'lib/helpers'
 require 'lib/sqs_client.rb'
 
 def handler(event:, context:)
-  puts event.inspect
-  thing = {
-    message: "foo bar"
-  }
-  $sqs_manager.enqueue(PLAYER_MESSAGES, thing.to_json)
+  Aws::DynamoDBStreams::AttributeTranslator
+    .from_event(event)
+    .each do |record|
+    $sqs_manager.enqueue(PLAYER_MESSAGES, record.to_h.to_json)
+  end
 end
