5f288562020b4b64764fdafaf342e23be8f5747a
Kenneth Berland <ken@hero.net>
2022-01-21 17:20:10 -0800

    fix specs

--- spec/auth/warp/post_spec.rb
+++ spec/auth/warp/post_spec.rb
@@ -7,17 +7,18 @@ require 'lib/helpers'
 RSpec.describe '#warp_post' do
   context 'lambda_result' do
     let(:discord_id) { seeded_random_integer($example_name).to_s }
+    let(:game_uuid) { SecureRandom.uuid }
+    let (:post_body) {""}
     let(:event) {
       {
-        'body' => File.read(post_file),
+        'body' => post_body,
         'pathParameters' => {
-          'proxy' => "#{discord_id}/foobar/#{SecureRandom.uuid}"
+          'proxy' => "#{discord_id}/game_uuid/#{game_uuid}"
         }
       }
     }
     
     let(:lambda_result) { auth_warp_post_handler(event: event, context: '') }
-    let(:post_file) {'spec/mocks/game/valid-duel-post.json'}
 
     before(:example) {
       response = File.read('spec/mocks/user/by-discord-id/dynamo-get-found.json')
@@ -32,29 +33,33 @@ RSpec.describe '#warp_post' do
       it_behaves_like 'lambda function'
     end
 
-    describe 'for response 400' do
-      let(:discord_id) {"invalid discord_id"}
-      it 'returns 400, invalid discord id' do
+    describe 'with invalid input' do
+
+      describe 'an invalid discord_id' do
+        let(:discord_id) {"invalid discord_id"}
+        it 'returns 400, invalid discord id' do
           expect(lambda_result[:statusCode]).to eq 400
+        end
       end
 
-      let(:invalid_uuid) {SecureRandom.uuid}
-      it 'returns 400, invalid game uuid' do
+      describe 'an invalid game uuid' do
+        let(:game_uuid) { 'invalid game uuid' }
+        it 'returns 400, invalid game uuid' do
           expect(lambda_result[:statusCode]).to eq 400
+        end
       end
 
-      let(:post_file) {'spec/mocks/game/valid-duel-post.json'}
-      it 'returns 400, post body length > 0' do
+      describe 'a post body for a method that does not require one' do
+        let(:post_body) { 'this should be empty' }
+        it 'returns 400, invalid post body length > 0' do
           expect(lambda_result[:statusCode]).to eq 400
+        end
       end
-    end
 
-    describe `for response 200` do
-      let(:discord_id) {"240177490906054658"}
-      let(:valid_uuid) {"87d2ebbf-3016-4ab8-97e6-c06e410b3359"}
-      let (:post_body) {""}
-      it 'returns 200, valid' do
+      describe 'for a proper request' do
+        it 'returns 200, valid' do
           expect(lambda_result[:statusCode]).to eq 200
+        end
       end
     end
   end
