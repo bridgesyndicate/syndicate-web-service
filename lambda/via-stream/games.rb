c84a13b4c1ddd50cb4d8add7f5beb993e4fc891c
Kenneth Berland <ken@hero.net>
2022-02-10 12:27:58 -0800

    remove redundant logging

--- lambda/via-stream/games.rb
+++ lambda/via-stream/games.rb
@@ -10,9 +10,7 @@ require 'lib/rabbit_client_factory'
 require 'lib/dynamo_client'
 
 def handler(event:, context:)
-  syn_logger event.class
   syn_logger event
-  syn_logger event.inspect
   Aws::DynamoDBStreams::AttributeTranslator
     .from_event(event)
     .each do |record|
