bd9d280d3e0cddf16546afd70ac3282939f59bbd
Vice Versa <vice@bridgesyndicate.gg>
2022-02-10 22:35:24 -0800

    add rescues so events don't process forever

--- lambda/via-stream/games.rb
+++ lambda/via-stream/games.rb
@@ -17,24 +17,42 @@ def handler(event:, context:)
     game_stream = GameStream.new(record)
     unless game_stream.ddb_insert?
       if game_stream.ddb_task_ip_modify?
-        syn_logger "sending new game sqs for #{game_stream.uuid} event: #{game_stream.event_id}"
-        $sqs_manager.enqueue(PLAYER_MESSAGES, game_stream.to_json)
+        begin
+          syn_logger "sending new game sqs for #{game_stream.uuid} event: #{game_stream.event_id}"
+          $sqs_manager.enqueue(PLAYER_MESSAGES, game_stream.to_json)
+        rescue Exception => e
+          syn_logger "Exception in game_stream.ddb_task_ip_modify?"
+          syn_logger e
+          syn_logger e.inspect
+        end
       elsif game_stream.game_aborted?
-        $sqs_manager.enqueue(PLAYER_MESSAGES, game_stream.to_json)
-        rabbit_client = RabbitClientFactory.produce
-        rabbit_client.clear_warp_cache_for_players(game_stream.player_uuids)
-        rabbit_client.shutdown
+        begin
+          $sqs_manager.enqueue(PLAYER_MESSAGES, game_stream.to_json)
+          rabbit_client = RabbitClientFactory.produce
+          rabbit_client.clear_warp_cache_for_players(game_stream.player_uuids)
+          rabbit_client.shutdown
+        rescue Exception => e
+          syn_logger "Exception in game_stream.game_aborted?"
+          syn_logger e
+          syn_logger e.inspect
+        end
       elsif game_stream.game_ended_with_score?
-        syn_logger "sending game end sqs for #{game_stream.uuid} event: #{game_stream.event_id}"
-        game_stream.compute_elo_changes
-        $sqs_manager.enqueue(PLAYER_MESSAGES, game_stream.to_json)
-        $ddb_user_manager.batch_update(game_stream.batch)
-        syn_logger "game #{game_stream.uuid} saved update user records"
-        game_stream.update_leaderboard
-        syn_logger "game #{game_stream.uuid} updated leaderboard"
-        rabbit_client = RabbitClientFactory.produce
-        rabbit_client.clear_warp_cache_for_players(game_stream.player_uuids)
-        rabbit_client.shutdown
+        begin
+          syn_logger "sending game end sqs for #{game_stream.uuid} event: #{game_stream.event_id}"
+          game_stream.compute_elo_changes
+          $sqs_manager.enqueue(PLAYER_MESSAGES, game_stream.to_json)
+          $ddb_user_manager.batch_update(game_stream.batch)
+          syn_logger "game #{game_stream.uuid} saved update user records"
+          game_stream.update_leaderboard
+          syn_logger "game #{game_stream.uuid} updated leaderboard"
+          rabbit_client = RabbitClientFactory.produce
+          rabbit_client.clear_warp_cache_for_players(game_stream.player_uuids)
+          rabbit_client.shutdown
+        rescue Exception => e
+          syn_logger "Exception in game_stream.game_ended_with_score?"
+          syn_logger e
+          syn_logger e.inspect
+        end
       end
     end
   end
