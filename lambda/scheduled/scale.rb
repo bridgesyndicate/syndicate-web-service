1b22f3ebdd232ff94d8ab7333f8b98a39b02974f
Kenneth Berland <ken@hero.net>
2022-08-07 15:25:55 -0700

    configure the ECSClient

--- lambda/scheduled/scale.rb
+++ lambda/scheduled/scale.rb
@@ -8,12 +8,17 @@ require 'lib/auto_scaler'
 
 def handler(event:, context:)
   puts "git sha is: #{$my_git_commit_sha}"
-  tasks = ECSClient
+
+  config = AppconfigClient.get_configuration
+  ecs_client = ECSClient.new(
+    tasks_subnet: config[:tasks_subnet],
+    tasks_security_group: config[:tasks_security_group]
+  )
+  tasks = ecs_client
     .list_tasks
     .task_arns
   CloudwatchClient.put_game_container_task_count(tasks.size)
   delay = CloudwatchClient.get_container_metadata_delay
-  config = AppconfigClient.get_configuration
   auto_scaler = AutoScaler.new(tasks, delay, config)
   auto_scaler.set_sql_client(PostgresClient.instance)
   auto_scaler.scale
