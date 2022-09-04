19319037a27c3e4e3c3561d3842255ec0bc5f0c5
Kenneth Berland <ken@hero.net>
2022-09-04 09:00:47 -0700

    checkpoint

---
+++ lambda/auth/ban/delete.rb
@@ -0,0 +1,28 @@
+load 'git_commit_sha.rb'
+require 'json'
+require 'lib/helpers'
+require 'lib/dynamo_client.rb'
+
+def auth_ban_delete_handler(event:, context:)
+
+  headers_list = {
+    "Access-Control-Allow-Origin" => "*",
+    "X-git-commit-sha" => $my_git_commit_sha
+  }
+
+  begin
+    minecraft_uuid = event['pathParameters']['proxy']
+    raise unless minecraft_uuid.match(UUID_REGEX)
+  rescue
+    return { statusCode: BAD_REQUEST,
+           headers: headers_list,
+           body: {}.to_json
+    }
+  end
+
+  ret = $ddb_user_manager.unban(minecraft_uuid)
+
+  return { statusCode: OK,
+           headers: headers_list,
+           body: ret.attributes.to_json }
+end
